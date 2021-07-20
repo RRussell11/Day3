0df4ec9b425f348d048b6448f692ac51b1d6d274
rrussell <rrtt479@gmail.com>
2021-07-19 20:30:06 -0400

    Add files via upload
    Datatypes

---
+++ Demo.java
@@ -0,0 +1,50 @@
+package datatypes;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//Variables 
+		//1.primitives
+		//2.reference type
+		
+		//Primitive Types
+		/*1.byte(8 bits)
+		 *2.short(16 bits)
+		 *3.chart(16 bits)
+		 *... 
+		 */
+		//Floating Point Types
+		/*6.float(32 bits)
+		 *7.double(64 bits)
+		 *8.boolean(true, false) 
+		 */
+
+		byte a1;
+		byte a2;
+		a1= 10;
+		a2= 15;
+		
+		System.out.println("a:" + a1);
+		System.out.println("b:" + a2);
+		byte a3= (byte) (a1 + a2);
+	
+		int b1= 100343432;
+		byte b2= (byte) b1;
+		System.out.println("b2:" + b2);
+		int c1;
+		int c2 =50;
+		c1 = c2;
+		double d2 =11/2; //result is an int
+		System.out.println("d2:"+ d2);
+		//d3=
+		//
+		
+		//increment , decrement
+		/*infix
+		 * 
+		 *postfix/
+		 */
+		
+	}
+
+}
